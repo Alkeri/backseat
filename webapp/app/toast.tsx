256e71654ca42010c8d2f21e3301b9725d6415e8
Jamal Hashim <jamalh110@gmail.com>
2023-08-26 17:32:31 -0400

    hide bottom toast

--- webapp/app/toast.tsx
+++ webapp/app/toast.tsx
@@ -5,7 +5,7 @@ export default function Toast() {
   const cookieStore = cookies();
   const isHidden = cookieStore.get('template-banner-hidden');
 
-  return isHidden ? null : (
+  return true ? null : (
     <div className="sticky rounded-2xl w-11/12 sm:w-[581px] h-40 sm:h-[80px] p-0.5 z-10 bottom-10 left-0 right-0 mx-auto">
       <div className="rounded-[14px] w-full h-full bg-gray-50 border border-gray-200 flex flex-col sm:flex-row items-center justify-center sm:justify-between space-y-3 sm:space-y-0 px-5">
         <p className="text-black text-[13px] font-mono w-[304px] h-10 flex items-center justify-center p-3">
